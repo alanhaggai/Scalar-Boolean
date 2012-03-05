0575c879392636cffb98d5ad77915842e704dcd8
Alan Haggai Alavi <alanhaggai@alanhaggai.org>
2012-03-05 23:23:02 +0530

    Overload '=='

--- lib/Scalar/Boolean/Value.pm
+++ lib/Scalar/Boolean/Value.pm
@@ -8,9 +8,10 @@ my $true  = bless \do { ( my $data = 1 ) }, __PACKAGE__;
 my $false = bless \do { ( my $data = 0 ) }, __PACKAGE__;
 
 use overload
-  '!' => sub { ${ $_[0] } ? $false : $true },
+  '!'    => sub { ${ $_[0] } ? $false : $true },
   'bool' => sub { ${ $_[0] } },
-  'eq'   => sub { ${ $_[0] } eq $_[1] ? $true : $false };
+  'eq'   => sub { ${ $_[0] } eq $_[1] ? $true : $false },
+  '=='   => sub { ${ $_[0] } == $_[1] ? $true : $false };
 
 sub boolean($) { $_[0] ? $true : $false }
 sub true()     { $true }
